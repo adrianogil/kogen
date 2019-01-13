c59880adf76d2d501b2cd7705be5f433d680fc1e
Adriano Gil <adrianogil.san@gmail.com>
2019-01-13 17:47:00 -0400

    Small refactoring

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -1,1 +1,8 @@
-alias kogen="python2 $KOGEN_DIR/kogen.py"
+if [ -z "$KOGEN_PYTHON_PATH" ]
+then
+    export KOGEN_PYTHON_PATH=$KOGEN_DIR/python/
+    export PYTHONPATH=$KOGEN_PYTHON_PATH:$PYTHONPATH
+fi
+
+alias kogen="python2 -m kogen.kogen"
+alias kogen-goseun-questions="python2 -m kogen.goseun_questions"
---
+++ src/python/kogen/goseun_questions.py
@@ -0,0 +1,27 @@
+import sys
+from simplegrammar.grammar import SimpleGrammar
+
+
+total_questions = 10
+if len(sys.argv) > 1:
+    total_questions = int(sys.argv[1])
+
+xgo = SimpleGrammar() \
+    .set_text("#xgo#")\
+    .add_tag("xgo", [\
+        "그것","이것","저것" \
+        ])
+
+
+goseun_questions = SimpleGrammar() \
+    .set_text("#goseun_question#")\
+    .add_tag("goseun_question", [\
+        "#xgo#은 무엇입니까?\n#xgo#은 #substantive#입니다." \
+        ])\
+    .add_tag("substantive", [
+        "교회", "책", "책상", "공책", "의자", "구두", "문", "사탕"
+        ])
+
+for q in xrange(0, total_questions):
+    print(goseun_questions.add_tag("xgo", [str(xgo)]))
+    print("\n")
